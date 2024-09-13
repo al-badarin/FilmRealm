59dc104b81b818c946d3f29bd098ba759758571f
al-badarin <jamalalbadarin@gmail.com>
2024-09-13 09:59:52 +0300

    add timestamps to models

--- src/models/Cast.js
+++ src/models/Cast.js
@@ -1,39 +1,46 @@
 const mongoose = require('mongoose');
 
-const castSchema = new mongoose.Schema({
+const castSchema = new mongoose.Schema(
+  {
     name: {
-        type: String, 
-        required: true,
+      type: String,
+      required: true,
     },
     age: {
-        type: Number, 
-        required: true,
-        max: 120,
-        min: 1,
+      type: Number,
+      required: true,
+      max: 120,
+      min: 1,
     },
     born: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     nameInMovie: {
-        type: String,
-        required: true,
-    }, 
+      type: String,
+      required: true,
+    },
     castImage: {
-        type: String,
-        required: true,
-        validate: {
-            validator(value) {
-                return /^https?:\/\//.test(value);
-            },
-            message: (props) => `${props.value} is invalid url for the castImage!`
-        }
+      type: String,
+      required: true,
+      validate: {
+        validator(value) {
+          return /^https?:\/\//.test(value);
+        },
+        message: (props) => `${props.value} is invalid url for the castImage!`,
+      },
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now, // Automatically set createdAt to current date
     },
     // movies: [{
     //     type: mongoose.Types.ObjectId,
     //     ref: 'Movie'
     // }]
-});
+  },
+  { timestamps: true }
+);
 
 const Cast = mongoose.model('Cast', castSchema);
 
--- src/models/Movie.js
+++ src/models/Movie.js
@@ -1,58 +1,65 @@
 const mongoose = require('mongoose');
 
-const movieSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-    minLength: 5,
-    match: /^[a-zA-Z0-9\s:,'-]+$/, // Allows letters, numbers, spaces, colons, commas, apostrophes, and hyphens
-  },
-  genre: {
-    type: String,
-    required: true,
-    lowercase: true,
-    minLength: 5,
-    match: /^[a-zA-Z0-9\s]+$/,
-  },
-  director: {
-    type: String,
-    required: true,
-    minLength: 5,
-    match: /^[a-zA-Z0-9\s:,'-]+$/, // Allows letters, numbers, spaces, colons, commas, apostrophes, and hyphens
-  },
-  year: {
-    type: Number,
-    required: true,
-    min: 1900,
-    max: 2024,
-  },
-  rating: {
-    type: Number,
-    required: true,
-    min: 1,
-    max: 5,
-  },
-  description: {
-    type: String,
-    required: true,
-    maxLength: 1000,
-  },
-  imageUrl: {
-    type: String,
-    required: true,
-    match: /^https?:\/\//,
-  },
-  casts: [
-    {
-      type: mongoose.Types.ObjectId,
-      ref: 'Cast',
+const movieSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      minLength: 2,
+      match: /^[a-zA-Z0-9\s:,'-]+$/, // Allows letters, numbers, spaces, colons, commas, apostrophes, and hyphens
     },
-  ],
-  owner: {
-    type: mongoose.Types.ObjectId,
-    ref: 'User',
+    genre: {
+      type: String,
+      required: true,
+      lowercase: true,
+      minLength: 5,
+      match: /^[a-zA-Z0-9\s:,'-]+$/, // Allows letters, numbers, spaces, colons, commas, apostrophes, and hyphens
+    },
+    director: {
+      type: String,
+      required: true,
+      minLength: 5,
+      match: /^[a-zA-Z0-9\s:,'-]+$/, // Allows letters, numbers, spaces, colons, commas, apostrophes, and hyphens
+    },
+    year: {
+      type: Number,
+      required: true,
+      min: 1900,
+      max: 2024,
+    },
+    rating: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+    },
+    description: {
+      type: String,
+      required: true,
+      maxLength: 1000,
+    },
+    imageUrl: {
+      type: String,
+      required: true,
+      match: /^https?:\/\//,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now, // Automatically set createdAt to current date
+    },
+    casts: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: 'Cast',
+      },
+    ],
+    owner: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+    },
   },
-});
+  { timestamps: true }
+);
 
 const Movie = mongoose.model('Movie', movieSchema);
 
