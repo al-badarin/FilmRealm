97e301d8b57e7d3dd13dfe5b7fb719dcc1fda61b
al-badarin <jamalalbadarin@gmail.com>
2024-09-13 09:16:30 +0300

    allow additional characters in Movie model

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -1,55 +1,57 @@
 const mongoose = require('mongoose');
 
 const movieSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-        minLength: 5,
-        match: /^[a-zA-Z0-9\s]+$/
+  title: {
+    type: String,
+    required: true,
+    minLength: 5,
+    match: /^[a-zA-Z0-9\s:,'-]+$/, // Allows letters, numbers, spaces, colons, commas, apostrophes, and hyphens
+  },
+  genre: {
+    type: String,
+    required: true,
+    lowercase: true,
+    minLength: 5,
+    match: /^[a-zA-Z0-9\s]+$/,
+  },
+  director: {
+    type: String,
+    required: true,
+    minLength: 5,
+    match: /^[a-zA-Z0-9\s:,'-]+$/, // Allows letters, numbers, spaces, colons, commas, apostrophes, and hyphens
+  },
+  year: {
+    type: Number,
+    required: true,
+    min: 1900,
+    max: 2024,
+  },
+  rating: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5,
+  },
+  description: {
+    type: String,
+    required: true,
+    maxLength: 1000,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+    match: /^https?:\/\//,
+  },
+  casts: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: 'Cast',
     },
-    genre: {
-        type: String,
-        required: true,
-        lowercase: true,
-        minLength: 5,
-        match: /^[a-zA-Z0-9\s]+$/
-    },
-    director: {
-        type: String,
-        required: true,
-        minLength: 5,
-        match: /^[a-zA-Z0-9\s]+$/
-    },
-    year: {
-        type: Number,
-        required: true,
-        min: 1900,
-        max: 2024,
-    },
-    rating: {
-        type: Number,
-        required: true,
-        min: 1,
-        max: 5,
-    },
-    description: {
-        type: String,
-        required: true,
-        maxLength: 1000,
-    },
-    imageUrl: {
-        type: String,
-        required: true,
-        match: /^https?:\/\//
-    },
-    casts: [{
-        type: mongoose.Types.ObjectId,
-        ref: 'Cast'
-    }],
-    owner: {
-        type: mongoose.Types.ObjectId,
-        ref: 'User'
-    }
+  ],
+  owner: {
+    type: mongoose.Types.ObjectId,
+    ref: 'User',
+  },
 });
 
 const Movie = mongoose.model('Movie', movieSchema);
